36539bde428ab32f9e3b59ebe71e17da374bb270
AdvancedAntiSkid <47271339+AdvancedAntiSkid@users.noreply.github.com>
2022-09-13 21:05:04 +0200

    Added annotations

---
+++ src/main/java/ac/fusion/framework/util/annotation/PostConfirmed.java
@@ -0,0 +1,14 @@
+package ac.fusion.framework.util.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Represents an annotation that indicates if the given serverside state was POST confirmed by the client.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface PostConfirmed {
+}
